6a5c79d4f0e6464b4ea98e3a3c9ee564fed3ba9e
Seb <github@seb.exse.net>
2011-11-21 13:25:09 +0100

    implemented delay with systick

--- firmware_master/main.c
+++ firmware_master/main.c
@@ -6,38 +6,32 @@
 #include "drivers/uart0.h"
 
 
-void delay_()
+volatile uint32_t timeout_ms = 0;
+volatile uint32_t timeout_delay_ms = 0;
+
+void SysTick_Handler(void) 
 {
-	uint16_t a=0;
-	uint16_t b=0;
-	for(a=0;a<0xFFFc;a++)
-	{
-		b++;
-		b++;
-		b++;
-		b++;
-		b++;
-		b++;
-		b++;
-		b++;
-		b++;
-		b++;
-		b++;
-		b++;
-		b++;
-		b++;
-		b++;
-		b++;
-	}
+	if(timeout_ms > 0) timeout_ms--;
+	if(timeout_delay_ms > 0) timeout_delay_ms--;
 }
 
+
+void delay_ms(uint32_t delay_period_ms)
+{
+	timeout_delay_ms = delay_period_ms;
+	while(timeout_delay_ms > 0);
+}
 
 
 int main(void) {
 	/* Your code goes here */
+	
 
 	// don't know why this is incorrect
 	SystemCoreClock = 100000000;
+	SysTick_Config(SystemCoreClock / 1000);
+
+	timeout_ms=0;
 
 	/* Turn on all of port 1 (this includes the 4 mbed LEDs) */
 	LPC_GPIO0->FIODIR |= (1<<19)|(1<<20);
@@ -50,16 +44,17 @@ int main(void) {
 	while(1)
 	{
 		LPC_GPIO0->FIOPIN |= (1<<19);
-		delay_();
+		delay_ms(100);
 		LPC_GPIO0->FIOPIN &= ~(1<<19);
-		delay_();
+		delay_ms(100);
 
-
-		char buf[100];
+		char buf[1000];
 
 		sprintf(buf,"System Core Clock is set to: %i \n",(int)SystemCoreClock);
-
 		UART0_PrintString(buf);
+		sprintf(buf,"timeout is at: %u \n",(unsigned int)timeout_ms);
+		UART0_PrintString(buf);
+
 		
 		if((LPC_GPIO0->FIOPIN & (1<<10)) == (1<<10))
 		{
@@ -70,6 +65,7 @@ int main(void) {
 		{
 			LPC_GPIO0->FIOPIN &= ~(1<<20);
 			UART0_PrintString("test2");
+			timeout_ms = 10000;
 		}
 	}
 }
