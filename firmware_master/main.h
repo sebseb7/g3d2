deccf69e13ea7788e8a19c46dae234cb3621f9b6
Seb <github@seb.exse.net>
2011-12-20 06:37:09 +0100

    symlinks to the game-files

---
+++ firmware_master/main.h
@@ -0,0 +1,28 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+enum {
+	DISPLAY_WIDTH = 72,
+	DISPLAY_HEIGHT = 32,
+};
+
+enum {
+	MODULES_PER_LINE = 9
+};
+
+enum {
+	BUTTON_UP,
+	BUTTON_DOWN,
+	BUTTON_LEFT,
+	BUTTON_RIGHT,
+	BUTTON_A,
+	BUTTON_B,
+	BUTTON_START,
+	BUTTON_SELECT,
+};
+
+int button_down(unsigned int button);
+unsigned int rand_int(unsigned int limit);
+void pixel(int x, int y, unsigned char color);
+
+#endif
---
+++ firmware_master/tetris.c
@@ -0,0 +1,1 @@
+../../tetrisVI/tetris.c
