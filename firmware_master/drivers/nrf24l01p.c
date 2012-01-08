b76595c6650270c46d6402f036fccc502f5e9aad
Seb <github@seb.exse.net>
2012-01-08 19:34:24 +0100

    space2tab

--- firmware_master/drivers/nrf24l01p.c
+++ firmware_master/drivers/nrf24l01p.c
@@ -10,7 +10,7 @@ uint8_t _nrfresets=0;
 /* Transmit a byte via SPI                                               */
 /*-----------------------------------------------------------------------*/
 inline void xmit_spi(uint8_t dat) {
-    sspSendByte (dat);
+	sspSendByte (dat);
 }
 
 
@@ -28,48 +28,48 @@ inline void xmit_spi(uint8_t dat) {
 
 
 void nrf_cmd(uint8_t cmd){
-    CS_LOW();
-    xmit_spi(cmd);
-    CS_HIGH();
+	CS_LOW();
+	xmit_spi(cmd);
+	CS_HIGH();
 };
 
 
 
 
 void nrf_write_reg(const uint8_t reg, const uint8_t val){
-    CS_LOW();
-    xmit_spi(C_W_REGISTER | reg);
-    xmit_spi(val);
-    CS_HIGH();
+	CS_LOW();
+	xmit_spi(C_W_REGISTER | reg);
+	xmit_spi(val);
+	CS_HIGH();
 };
 
 
 
 void nrf_write_long(const uint8_t cmd, int len, const uint8_t* data){
-    CS_LOW();
-    xmit_spi(cmd);
-    sspSend(data,len);
-    CS_HIGH();
+	CS_LOW();
+	xmit_spi(cmd);
+	sspSend(data,len);
+	CS_HIGH();
 };
 
 #define nrf_write_reg_long(reg, len, data) \
-    nrf_write_long(C_W_REGISTER|(reg), len, data)
+	nrf_write_long(C_W_REGISTER|(reg), len, data)
 
 
 
 
 void nrf_set_rx_mac(int pipe, int rxlen, int maclen, const uint8_t * mac){
 #ifdef SAFE
-    assert(maclen>=1 || maclen<=5);
-    assert(rxlen>=1 || rxlen<=32);
-    assert(pipe>=0 || pipe<=5);
-    assert(mac!=NULL);
-    if(pipe>1)
-        assert(maclen==1);
+	assert(maclen>=1 || maclen<=5);
+	assert(rxlen>=1 || rxlen<=32);
+	assert(pipe>=0 || pipe<=5);
+	assert(mac!=NULL);
+	if(pipe>1)
+		assert(maclen==1);
 #endif
-    nrf_write_reg(R_RX_PW_P0+pipe,rxlen);
+	nrf_write_reg(R_RX_PW_P0+pipe,rxlen);
 
-    nrf_write_reg_long(R_RX_ADDR_P0+pipe,maclen,mac);
+	nrf_write_reg_long(R_RX_ADDR_P0+pipe,maclen,mac);
 //    nrf_write_reg(R_EN_RXADDR, 
 //            nrf_read_reg(R_EN_RXADDR) | (1<<pipe)
 //            );
@@ -77,32 +77,32 @@ void nrf_set_rx_mac(int pipe, int rxlen, int maclen, const uint8_t * mac){
 
 void nrf_set_tx_mac(int maclen, const uint8_t * mac){
 #ifdef SAFE
-    assert(maclen>=1 || maclen<=5);
-    assert(mac!=NULL);
+	assert(maclen>=1 || maclen<=5);
+	assert(mac!=NULL);
 #endif
-    nrf_write_reg_long(R_TX_ADDR,maclen,mac);
+	nrf_write_reg_long(R_TX_ADDR,maclen,mac);
 };
 
 
 void nrf_set_channel(int channel){
 #ifdef SAFE
-    assert(channel &~R_RF_CH_BITS ==0);
+	assert(channel &~R_RF_CH_BITS ==0);
 #endif
-    nrf_write_reg(R_RF_CH, channel);
+	nrf_write_reg(R_RF_CH, channel);
 };
 
 void nrf_config_set(nrfconfig config){
-    nrf_write_reg(R_SETUP_AW,R_SETUP_AW_5);
+	nrf_write_reg(R_SETUP_AW,R_SETUP_AW_5);
 
-    nrf_set_channel(config->channel);
+	nrf_set_channel(config->channel);
 
-    for(int i=0;i<config->nrmacs;i++){
-        nrf_write_reg(R_RX_PW_P0+i,config->maclen[i]);
-        if(i==0){
-            nrf_write_reg_long(R_RX_ADDR_P0,5,config->mac0);
-        }else if(i==1){
-            nrf_write_reg_long(R_RX_ADDR_P1,5,config->mac1);
-        }else if(i>1){
+	for(int i=0;i<config->nrmacs;i++){
+		nrf_write_reg(R_RX_PW_P0+i,config->maclen[i]);
+		if(i==0){
+			nrf_write_reg_long(R_RX_ADDR_P0,5,config->mac0);
+		}else if(i==1){
+			nrf_write_reg_long(R_RX_ADDR_P1,5,config->mac1);
+		}else if(i>1){
 			nrf_write_reg_long(R_RX_ADDR_P0+i,1,config->mac2345+i-2);
 		};
 	};
